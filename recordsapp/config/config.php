ededfb2fa2c053baf266f8bb41d8216990232fde
kenlorenz <kenlorenz420@gmail.com>
2023-11-06 21:47:05 +0800

    finishing tables

--- recordsapp/config/config.php
+++ recordsapp/config/config.php
@@ -1,10 +1,9 @@
 <?php
 
-define('ROOT_URL','http://127.0.0.1/recordsapp/');
-define('DB_HOST', '127.0.0.1');
+define('ROOT_URL','http://localhost/recordsapp/');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'ren');
 define('DB_PASS', '122846');
 define('DB_NAME', 'recordsapp_db');
 
 
-?>
