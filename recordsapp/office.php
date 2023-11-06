aac926668b23440124f47da7be063c554a31009c
kenlorenz <kenlorenz420@gmail.com>
2023-11-07 00:35:55 +0800

    pagination completed

--- recordsapp/office.php
+++ recordsapp/office.php
@@ -40,6 +40,8 @@
         $page = $_GET['page'];
     }
     
+    // determine the sql LIMIT starting number for the results on the display page
+    $page_first_result = ($page-1) * $results_per_page;
     
     # create query
     $query = 'SELECT * FROM office ORDER BY name LIMIT '. $page_first_result . ',' . $results_per_page;
