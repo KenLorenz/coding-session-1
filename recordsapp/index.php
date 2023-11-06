12ed52a9eb3c2b667fae8b728dc143ec7a622c6c
kenlorenz <kenlorenz420@gmail.com>
2023-11-06 23:02:53 +0800

    Add Office Feature finished.

--- recordsapp/index.php
+++ recordsapp/index.php
@@ -20,14 +20,14 @@
 
 <body>
     <div class="wrapper">
-        <div class="sidebar" data-image="../assets/img/sidebar-5.jpg">1
+        <div class="sidebar" data-image="../assets/img/sidebar-5.jpg">
             <!--
         Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
 
         Tip 2: you can also add an image using data-image tag
     -->
             <div class="sidebar-wrapper">
-                <?php include('includes/sidebar.php'); ?>
+                <?php include('includes/sidebar.php') ?>
                 <!-- 1 -->
             </div>
         </div>
