ef99e1522020527f235b188ce20a275123901a32
kenlorenz <kenlorenz420@gmail.com>
2023-11-09 19:03:35 +0800

    added comments to prepare for new delete feature

--- recordsapp/office-delete.php
+++ recordsapp/office-delete.php
@@ -11,4 +11,8 @@ if(mysqli_query($conn, $query)){
     echo 'ERROR: '.mysqli_error($conn);
 }
 
+# will do later
+# moves transations office_id that had the deleted office
+# moves employee that had the deleted office
+
 header("Location:index.php");
