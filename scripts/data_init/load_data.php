adc85ae644f2ba6b04fc65469ac1fa8b5a78fc5d
kenlorenz <kenlorenz420@gmail.com>
2023-11-09 17:20:37 +0800

    organizing the codes

--- scripts/data_init/load_data.php
+++ scripts/data_init/load_data.php
@@ -1,71 +1,84 @@
 <?php
 
 require ('scripts/data_init/vendor/autoload.php');
+require ('config/config.php');
 
 $faker = Faker\Factory::create('en_PH');
 
-$conn = mysqli_connect("localhost", "ren", "122846", "recordsapp_db", 3307); 
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_PASS, 3307);
 
-# transaction
-for($i = 1 ;$i <= 500; $i++){
-    $newtime = $faker->dateTime($max = 'now', $timezone = null);
-    $newtime = $newtime->format('Y-m-d H:i:s');
-    
-    /* $arr = array('IN','OUT','COMPLETE');
-    $ok = $arr[rand(0,2)]; */
-    
-    $query = "SELECT id from employee order by id desc LIMIT 1";
-    $result = mysqli_query($conn, $query);
-
-    while($x = mysqli_fetch_array($result)){
-        $emp = $faker->numberBetween($min = 1, $x['id']);    
-    }
-    
-
-    $query = "SELECT id from office order by id desc LIMIT 1";
-    $result = mysqli_query($conn, $query);
-    
-
-    while($x = mysqli_fetch_array($result)){
-        $off = $faker->numberBetween($min = 1, $x['id']);
-    }
-
-    $sql = "INSERT INTO recordsapp_db.transaction(`employee_id`,`office_id`,`datelog`,`action`,`remarks`,`documentcode`) VALUES('$emp','$off','$newtime','".$faker->randomElement(['IN','OUT','COMPLETE'])."','$faker->word','$faker->numberBetween($min = 100, $max = 400)');";
-
-
-    $test = mysqli_query($conn, $sql); # since mysql hates some faker generation, we let iteration rerun.
-}
-
-# employee
-for($i = 1 ;$i <= 200; $i++){
-    $query = "SELECT id from office order by id desc LIMIT 1";
-    $result = mysqli_query($conn, $query);
-
-    
-    while($x = mysqli_fetch_array($result)){
-        $off = $faker->numberBetween($min = 1, $x['id']);
-    }
-
-    $sql = "INSERT INTO recordsapp_db.employee(`lastName`,`firstName`,`office_id`,`address`) VALUES('$faker->lastName','$faker->firstName','$off','$faker->address');";
-
-    try{
-        $test = mysqli_query($conn, $sql); # since mysql hates some faker generation, we let iteration rerun.
-    } catch(Exception $e){
-        $i--;
+function iteration_reset($conn): void{ # likely to delete
+    foreach (['office','employee','transaction'] as $x){
+        $query = "alter table ".$x." auto_increment = 1";
+        mysqli_query($conn, $query);
     }
 }
 
-# office
+function faker_office($faker,$conn): void {
+    for($i = 1 ;$i <= 50; $i++){
+        $sql = "INSERT INTO recordsapp_db.office(`name`,`contactnum`,`email`,`address`,`city`,`country`,`postal`) VALUES
+        ('$faker->company','$faker->phoneNumber','$faker->email','$faker->address','$faker->city','$faker->country','$faker->postcode');";
+    
+        try{
+            mysqli_query($conn, $sql); # since mysql hates some faker generation, we let iteration rerun.
+        } catch(Exception $e){
+            $i--;
+        }
+    }
 
-for($i = 1 ;$i <= 50; $i++){
-    $sql = "INSERT INTO recordsapp_db.office(`name`,`contactnum`,`email`,`address`,`city`,`country`,`postal`) VALUES('$faker->company','$faker->phoneNumber','$faker->email','$faker->address','$faker->city','$faker->country','$faker->postcode');";
+}
 
-    try{
-        $test = mysqli_query($conn, $sql); # since mysql hates some faker generation, we let iteration rerun.
-    } catch(Exception $e){
-        $i--;
+function faker_employee($faker,$conn): void {
+    for($i = 1 ;$i <= 200; $i++){
+        $query = "SELECT id from office order by id desc LIMIT 1";
+        $result = $conn -> query($conn, $query);
+    
+        while($x = $conn -> fetch_array($result)){
+            $off = $faker->numberBetween($min = 1, $x['id']);
+        }
+    
+        $sql = "INSERT INTO recordsapp_db.employee(`lastName`,`firstName`,`office_id`,`address`) VALUES
+        ('$faker->lastName','$faker->firstName','$off','$faker->address');";
+    
+        try{
+            mysqli_query($conn, $sql); # since mysql hates some faker generation, we let iteration rerun.
+        } catch(Exception $e){
+            $i--;
+        }
     }
 }
+
+function faker_transaction($faker,$conn): void {
+    for($i = 1 ;$i <= 500; $i++){
+        $newtime = $faker->dateTime($max = 'now', $timezone = null);
+        $newtime = $newtime->format('Y-m-d H:i:s');
+        
+        $query = "SELECT id from employee order by id desc LIMIT 1";
+        $result = mysqli_query($conn, $query);
+    
+        while($x = mysqli_fetch_array($result)){
+            $emp = $faker->numberBetween($min = 1, $x['id']);    
+        }
+        
+        $query = "SELECT id from office order by id desc LIMIT 1";
+        $result = mysqli_query($conn, $query);
+        
+    
+        while($x = mysqli_fetch_array($result)){
+            $off = $faker->numberBetween($min = 1, $x['id']);
+        }
+    
+        $sql = "INSERT INTO recordsapp_db.transaction(`employee_id`,`office_id`,`datelog`,`action`,`remarks`,`documentcode`) VALUES
+        ('$emp','$off','$newtime','".$faker->randomElement(['IN','OUT','COMPLETE'])."','$faker->word','$faker->numberBetween($min = 100, $max = 400)');";
+    
+    
+        mysqli_query($conn, $sql); # since mysql hates some faker generation, we let iteration rerun.
+    }
+}
+#iteration_reset($conn); # temporary
+faker_office($faker,$conn);
+faker_employee($faker,$conn);
+#faker_transaction($faker,$conn);
 
 mysqli_close($conn);
 
